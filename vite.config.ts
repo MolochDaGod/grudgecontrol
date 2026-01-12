71a4bcb7c07b99f4c0e4a661213cfebc0404431a
2452327415qh-debug <2452327415qh@gmail.com>
2026-01-13 00:09:28 +0800

    新增交互事件开启与关闭

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "./example",
-  base: "./glbScene.html",
+  base: "./",
   build: {
     outDir: "../docs",
     emptyOutDir: true,
