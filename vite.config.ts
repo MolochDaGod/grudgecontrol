78787ebca76fd641582d2f7bb7a057740b127204
hang <hang.email@example.com>
2026-01-22 17:11:58 +0800

    添加<3dtiles 自定义>示例

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,17 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/three-player-controller/",
-  root: resolve(__dirname, "example"),
-  build: {
-    outDir: resolve(__dirname, "docs"),
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "example", "index.html"),
-        tiles: resolve(__dirname, "example", "3dtilesScene.html"),
-      },
+    base: "/three-player-controller/",
+    root: resolve(__dirname, "example"),
+    build: {
+        outDir: resolve(__dirname, "docs"),
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "example", "index.html"),
+                tiles: resolve(__dirname, "example", "3dtilesScene.html"),
+                tilesCustomize: resolve(__dirname, "example", "3dtilesCustomize.html"),
+            },
+        },
     },
-  },
 });
